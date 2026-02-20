a543247ed6a27f074774b708756bf0f27ee8eeca
higor <higoraugustorj@gmail.com>
2026-02-20 17:21:08 -0300

    feat: Email Sender Controller

--- src/main/java/com/higordev/email_service/controllers/EmailSenderController.java
+++ src/main/java/com/higordev/email_service/controllers/EmailSenderController.java
@@ -1,6 +1,13 @@
 package com.higordev.email_service.controllers;
 
 import com.higordev.email_service.application.EmailSenderService;
+import com.higordev.email_service.core.EmailRequest;
+import com.higordev.email_service.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailSenderController {
     private final EmailSenderService emailSenderService;
 
+    @Autowired
     public EmailSenderController(EmailSenderService emailSenderService) {
         this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("Email sent succesfully");
+        } catch (EmailServiceException exception) {
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email");
+        }
     }
 }
---
+++ src/main/java/com/higordev/email_service/core/EmailRequest.java
@@ -0,0 +1,3 @@
+package com.higordev.email_service.core;
+
+public record EmailRequest(String to, String subject, String body) {}
