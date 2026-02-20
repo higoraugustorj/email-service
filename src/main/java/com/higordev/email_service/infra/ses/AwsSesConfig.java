eb49ad8c675a024182c17e406abc9cfa004063f3
higor <higoraugustorj@gmail.com>
2026-02-20 17:58:26 -0300

    AwsSesConfig ajustes

--- src/main/java/com/higordev/email_service/infra/ses/AwsSesConfig.java
+++ src/main/java/com/higordev/email_service/infra/ses/AwsSesConfig.java
@@ -1,14 +1,33 @@
 package com.higordev.email_service.infra.ses;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsSesConfig {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.accessKeyID}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+        // Criando as credenciais explicitamente com os dados do properties
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withRegion(region) // Define a região sa-east-1
+                .withCredentials(new AWSStaticCredentialsProvider(credentials)) // Define as chaves
+                .build();
     }
 }
